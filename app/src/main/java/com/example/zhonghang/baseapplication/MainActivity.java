12f53ff6eccaa93869eb7ad9b1f2cd58049487d0
zhonghang <zhonghangIT@163.com>
2018-11-08 16:38:28 +0800

    添加了glide加载图片的显示方式

--- app/src/main/java/com/example/zhonghang/baseapplication/MainActivity.java
+++ app/src/main/java/com/example/zhonghang/baseapplication/MainActivity.java
@@ -2,27 +2,25 @@ package com.example.zhonghang.baseapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.example.frescolibrary.FrescoImageUtils;
-import com.facebook.drawee.view.SimpleDraweeView;
+import com.example.zhonghang.baseapplication.media.image.GlideApp;
 
 /**
  * @author zhonghang
  */
 public class MainActivity extends AppCompatActivity {
-    private SimpleDraweeView draweeView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        draweeView = findViewById(R.id.draweeview);
+        imageView = findViewById(R.id.imageview);
 
 //        ![](https://ws3.sinaimg.cn/large/006tNbRwly1fwz8lyciwaj30hq0vkgmd.jpg)
-        FrescoImageUtils.getInstance()
-                .createWebImageParamsBuilder(getApplicationContext(), "https://ws3.sinaimg.cn/large/006tNbRwly1fwz8lyciwaj30hq0vkgmd.jpg")
-                .build().showWebImage(draweeView);
-
+        GlideApp.with(this).load("https://ws3.sinaimg.cn/large/006tNbRwly1fwz8lyciwaj30hq0vkgmd.jpg").into(imageView);
     }
 
 }
